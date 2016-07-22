a16cce4ee7fde1f54cb16533bc2c9ca47640841d
Sindre Sorhus <sindresorhus@gmail.com>
2016-07-22 23:18:31 +0200

    init

---
+++ test.js
@@ -0,0 +1,14 @@
+import test from 'ava';
+import m from './';
+
+test('async', async t => {
+	t.is(await m('fixtures/noop.txt'), 'fixtures/noop.txt');
+	t.is(await m('fixtures/unicorn.txt'), 'fixtures/unicorn (1).txt');
+	t.is(await m('fixtures/rainbow.txt'), 'fixtures/rainbow (3).txt');
+});
+
+test('sync', t => {
+	t.is(m.sync('fixtures/noop.txt'), 'fixtures/noop.txt');
+	t.is(m.sync('fixtures/unicorn.txt'), 'fixtures/unicorn (1).txt');
+	t.is(m.sync('fixtures/rainbow.txt'), 'fixtures/rainbow (3).txt');
+});
