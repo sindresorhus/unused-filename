910394fdd0a3ee7dd5674de1e3b109983507b7cb
fisker <lionkay@gmail.com>
2019-07-07 02:36:58 +0800

    index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,12 @@
+declare namespace unusedFilename {
+	interface Options {
+		/**
+		Filename increase function
+		*/
+		incrementer?: (filename: string, extension: string, counter: number) => string;
+	}
+}
+
 declare const unusedFilename: {
 	/**
 	Get an unused filename by appending a number if it exists: `file.txt` → `file (1).txt`.
@@ -5,7 +14,7 @@ declare const unusedFilename: {
 	@param filePath - The path to check for filename collision.
 	@returns Either the original `filename` or the `filename` appended with a number.
 	*/
-	(filePath: string): Promise<string>;
+	(filePath: string, options?: unusedFilename.Options): Promise<string>;
 
 	/**
 	Synchronously get an unused filename by appending a number if it exists: `file.txt` → `file (1).txt`.
@@ -13,7 +22,7 @@ declare const unusedFilename: {
 	@param filePath - The path to check for filename collision.
 	@returns Either the original `filename` or the `filename` appended with a number.
 	*/
-	sync(filePath: string): string;
+	sync(filePath: string, options?: unusedFilename.Options): string;
 
 	// TODO: Remove this for the next major release
 	default: typeof unusedFilename;
