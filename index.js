bbf0d92074ed757add8e68bd7c6466280e35c5e2
tomasklaen <tomas.klaen@gmail.com>
2021-05-07 21:57:44 +0200

    Escape separator and use capture groups

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 'use strict';
 const pathExists = require('path-exists');
 const modifyFilename = require('modify-filename');
+const escapeStringRegexp = require('escape-string-regexp');
 
-const parenthesesIncrementer = (filename, extension) => {
-	let [, originalFilename, index] = filename.match(/^(.*)\((\d+)\)$/) || [null, filename, 0];
-	return `${originalFilename.trim()} (${++index})${extension}`;
+const parenthesesIncrementer = (inputFilename, extension) => {
+	const match = inputFilename.match(/^(?<filename>.*)\((?<index>\d+)\)$/);
+	let {filename, index} = match ? match.groups : {filename: inputFilename, index: 0};
+	return `${filename.trim()} (${++index})${extension}`;
 };
 
-const separatorIncrementer = separator => (filename, extension) => {
-	let [, originalFilename, index] = new RegExp(`^(.*)${separator}(\\d+)$`).exec(filename) || [null, filename, 0];
-	return `${originalFilename.trim()}_${++index}${extension}`;
+const separatorIncrementer = separator => {
+	const escapedSeparator = escapeStringRegexp(separator);
+
+	return (inputFilename, extension) => {
+		const match = new RegExp(`^(?<filename>.*)${escapedSeparator}(?<index>\\d+)$`).exec(inputFilename);
+		let {filename, index} = match ? match.groups : {filename: inputFilename, index: 0};
+		return `${filename.trim()}_${++index}${extension}`;
+	};
 };
 
 const unusedFilename = async (filePath, {incrementer = parenthesesIncrementer, maxTries = Infinity} = {}) => {
